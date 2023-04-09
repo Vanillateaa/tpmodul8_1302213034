9fae60fccc122ffd56ca39bff145ca24cc093ea6
Vanillateaa <mohamad.william24@gmail.com>
2023-04-10 03:17:03 +0700

    menyelesaikan program dengan runtime configuration

--- tpmodul8_1302213034/Program.cs
+++ tpmodul8_1302213034/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using tpmodul8_1302213034;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+        Console.Write("Berapa suhu badan anda saat ini? Dalam " + config.config.satuan_suhu + ": ");
+        double suhu = double.Parse(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?: ");
+        int hariDemam = int.Parse(Console.ReadLine());
+
+        if (config.IsInputValid(suhu, hariDemam))
+        {
+            Console.WriteLine(config.config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.config.pesan_ditolak);
+
+
+        }
+    }
+}
